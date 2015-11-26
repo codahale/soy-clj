2bd9e367da40e8a9a1dcce4a7f4c1ee265b21717
Coda Hale <coda.hale@gmail.com>
2015-11-26 13:03:48 -0800

    Always specify a cache for the benchmarks.

--- test/soy_clj/bench_test.clj
+++ test/soy_clj/bench_test.clj
@@ -11,24 +11,27 @@
 
 (deftest ^:bench parse-cached-bench
   (println "\n\n=== Parsing (cached)")
-  (soy/set-cache (cache/lru-cache-factory {}))
+  (soy/set-cache (cache/lu-cache-factory {}))
   (soy/parse "example.soy")
   (c/bench (soy/parse "example.soy")))
 
 (deftest ^:bench render-big-bench
   (println "\n\n=== Rendering (big list)")
+  (soy/set-cache (cache/lu-cache-factory {}))
   (let [templates (soy/parse "example.soy")
         data {:items (vec (map str (range 1 1000)))}]
     (c/bench (soy/render templates "examples.simple.list" data))))
 
 (deftest ^:bench render-small-bench
   (println "\n\n=== Rendering (small list)")
+  (soy/set-cache (cache/lu-cache-factory {}))
   (let [templates (soy/parse "example.soy")
         data {:items (vec (map str (range 1 50)))}]
     (c/bench (soy/render templates "examples.simple.list" data))))
 
 (deftest ^:bench render-simple-bench
   (println "\n\n=== Rendering (simple)")
+  (soy/set-cache (cache/lu-cache-factory {}))
   (let [templates (soy/parse "example.soy")
         data {:bar "bar"}]
     (c/bench (soy/render templates "examples.simple.basic" data))))
