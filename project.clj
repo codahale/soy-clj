1ab97433ee1a6a6dddb2db0d3b92268f26fab932
Coda Hale <coda.hale@gmail.com>
2015-11-26 10:51:07 -0800

    Move Clojure dep to dev profile.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/codahale/soy-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC2"]
-                 [org.clojure/core.cache "0.6.4"]
+  :dependencies [[org.clojure/core.cache "0.6.4"]
                  [com.google.template/soy "2015-04-10"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0-RC2"]]}}
   :plugins [[codox "0.9.0"]])
