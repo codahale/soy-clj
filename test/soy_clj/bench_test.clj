5e4c863e08135721a9976bbd002d4b03ece9c0bc
Coda Hale <coda.hale@gmail.com>
2015-11-28 12:52:15 -0800

    Tidy up benchmarks.

--- test/soy_clj/bench_test.clj
+++ test/soy_clj/bench_test.clj
@@ -4,34 +4,35 @@
             [criterium.core :as c]
             [soy-clj.core :as soy]))
 
+(defn- sep
+  [s]
+  (printf "\n\n######  %s  ######\n" s))
+
 (deftest ^:bench parse-uncached-bench
-  (println "\n\n=== Parsing (uncached)")
+  (sep "Parsing (uncached)")
   (soy/set-cache (cache/ttl-cache-factory {} :ttl 0))
   (c/bench (soy/parse "example.soy")))
 
 (deftest ^:bench parse-cached-bench
-  (println "\n\n=== Parsing (cached)")
+  (sep "Parsing (cached)")
   (soy/set-cache (cache/lu-cache-factory {}))
   (soy/parse "example.soy")
   (c/bench (soy/parse "example.soy")))
 
 (deftest ^:bench render-big-bench
-  (println "\n\n=== Rendering (big list)")
-  (soy/set-cache (cache/lu-cache-factory {}))
+  (sep "Rendering (big list)")
   (let [templates (soy/parse "example.soy")
         data {:items (vec (map str (range 1 1000)))}]
     (c/bench (soy/render templates "examples.simple.list" data))))
 
 (deftest ^:bench render-small-bench
-  (println "\n\n=== Rendering (small list)")
-  (soy/set-cache (cache/lu-cache-factory {}))
+  (sep "Rendering (small list)")
   (let [templates (soy/parse "example.soy")
         data {:items (vec (map str (range 1 50)))}]
     (c/bench (soy/render templates "examples.simple.list" data))))
 
 (deftest ^:bench render-simple-bench
-  (println "\n\n=== Rendering (simple)")
-  (soy/set-cache (cache/lu-cache-factory {}))
+  (sep "Rendering (simple)")
   (let [templates (soy/parse "example.soy")
         data {:bar "bar"}]
     (c/bench (soy/render templates "examples.simple.basic" data))))
