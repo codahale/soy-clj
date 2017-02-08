5410429a93552c865f97b23e7cbf9b6540fa7caa
Coda Hale <coda.hale@gmail.com>
2017-02-08 08:13:57 -0800

    Version 0.3.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.codahale/soy-clj "0.3.3-SNAPSHOT"
+(defproject com.codahale/soy-clj "0.3.3"
   :description "An idiomatic Clojure wrapper for Google's Closure templating system."
   :url "https://github.com/codahale/soy-clj"
   :license {:name "Eclipse Public License"
